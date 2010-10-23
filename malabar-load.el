bcb5675aa835dfaf338c9e3c9bc11f46027acbf6
K. Adam Christensen <pope@shifteleven.com>
2010-10-23 15:20:28 -0400

    Fixing my malabar autoload and I'm relying on Emacs23

--- malabar-load.el
+++ malabar-load.el
@@ -1,12 +1,13 @@
-(load-file (concat vendor-dir "cedet/common/cedet.el"))
-(semantic-load-enable-minimum-features)
-(semantic-load-enable-code-helpers)
-(global-ede-mode t)
-
+(add-to-list 'load-path (concat vendor-dir "malabar/lisp"))
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                  global-semanticdb-minor-mode
+                                  global-semantic-idle-summary-mode
+                                  global-semantic-mru-bookmark-mode))
+(semantic-mode 1)
 (require 'malabar-mode)
 (setq malabar-groovy-lib-dir (concat vendor-dir "malabar/lib"))
+(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
 (add-hook 'malabar-mode-hook
           '(lambda ()
             (add-hook 'after-save-hook 'malabar-compile-file-silently
                       nil t)))
-(require 'ecb)

958227f73c043da239d85fbdc9a80223292556c8
K. Adam Christensen <pope@shifteleven.com>
2011-02-16 20:46:45 -0500

    More php-flymake cleanup.
    flymake + hooks + mumamo = ugh

--- php-flymake.el
+++ php-flymake.el
@@ -8,23 +8,21 @@
 	 (local (file-relative-name temp (file-name-directory buffer-file-name))))
     (list "php" (list "-f" local "-l"))))
 
+(defvar *php-file-regex*
+  "\\.\\(php[s34]?\\|phtml\\|inc\\|module\\|install\\|engine\\)")
+
 ;;;###autoload
 (defun php/flymake-enable ()
-  (require 'flymake)
-  ;; TODO check that file ends with PHP
-  (flymake-mode 1))
+  (if (string-match-p *php-file-regex* (buffer-file-name))
+      (progn
+        (require 'flymake)
+        (flymake-mode 1))))
 
 (eval-after-load 'flymake
-  '(progn
+  `(progn
      (add-to-list 'flymake-err-line-patterns
                   '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.php[s34]?$" php-flymake))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.phtml$" php-flymake))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.inc$" php-flymake))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.module$" php-flymake))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.install$" php-flymake))
-     (add-to-list 'flymake-allowed-file-name-masks '("\\.engine$" php-flymake))
-
+     (add-to-list 'flymake-allowed-file-name-masks '(,*php-file-regex* php-flymake))
      (eval-after-load 'php-mode
        '(progn
           (define-key php-mode-map '[M-S-up] 'flymake-goto-prev-error)
